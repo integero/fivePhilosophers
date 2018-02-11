282f027dba4ff9bc37f148e431b286ffcc4571b7
Valeryi Tselishchev <var.tsel@gmail.com>
2018-02-11 14:16:33 +0300

    Revert "After cleaning from git-absent version"
                                                                 This reverts commit f764dd01857fb8a41418217543ed43a698345384.

--- src/com/five_philosophers/Sout.java
+++ src/com/five_philosophers/Sout.java
@@ -1,7 +1,7 @@
 package com.five_philosophers;
 
-public class Sout {
-    public static void prnt(int nTime, String sWhat) {
+class Sout {
+    static void prnt(int nTime, String sWhat) {
         String[] sout = {"%20s\n", "%40s\n", "%60s\n", "%80s\n","%100s\n"};
         System.out.printf(sout[nTime],sWhat);
     }
