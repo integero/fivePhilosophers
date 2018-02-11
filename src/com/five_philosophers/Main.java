d953332e2c1567352ceb82909904829d4ff21c56
Valeryi Tselishchev <var.tsel@gmail.com>
2018-02-11 14:30:49 +0300

    replaced value of timeForSpeach in class Main

--- src/com/five_philosophers/Main.java
+++ src/com/five_philosophers/Main.java
@@ -3,6 +3,6 @@ package com.five_philosophers;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
-        new FiveOClock(10,1000,5,7);
+        new FiveOClock(50,1000,5,7);
     }
 }
